51c648342f9978857ee52ed5f6de59ee0192a766
IQ-7 <iq7.dev@gmail.com>
2023-04-09 08:47:55 +0000

    NATSU: IMPORT SCRIPT [SKIP CI]

---
+++ build.sh
@@ -0,0 +1,22 @@
+# sync rom
+repo init --depth=1 --no-repo-verify -u https://github.com/NusantaraProject-ROM/android_manifest -b 10 -g default,-mips,-darwin,-notdefault
+git clone https://github.com/natssuu88/local_manifest --depth 1 -b nad-10-ginkgo .repo/local_manifests
+repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
+
+# build rom
+source $CIRRUS_WORKING_DIR/config
+timeStart
+
+source build/envsetup.sh
+export BUILD_USERNAME=zacky
+export BUILD_HOSTNAME=android-build
+lunch nad_ginkgo-userdebug
+mkfifo reading # Jangan di Hapus
+tee "${BUILDLOG}" < reading & # Jangan di Hapus
+build_message "Building Started" # Jangan di Hapus
+progress & # Jangan di Hapus
+mka nad -j8  > reading & sleep 95m # Jangan di hapus text line (> reading)
+
+retVal=$?
+timeEnd
+statusBuild
